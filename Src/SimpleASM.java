0ae269a274e3de8e17daf96a51fde294cd0d531b
leonardoaraujo.santos <leonardoaraujo.santos@gmail.com>
2011-08-08 01:35:47 +0200

    Working on...

--- Src/SimpleASM.java
+++ Src/SimpleASM.java
@@ -9,8 +9,25 @@ public class SimpleASM {
 	 * @param args
 	 */
 	public static void main(String[] args) throws Exception {
-		// TODO Auto-generated method stub
-		SimpleASMLexer lex = new SimpleASMLexer(new ANTLRFileStream(args[0], "UTF8"));
+		// Get input and pass to our lexer
+		System.out.println("Compiling source... " + args[0]);
+		SimpleASMLexer lex = new SimpleASMLexer(new ANTLRFileStream(args[0]));
+		
+		// Create tokens from lexer....
+		CommonTokenStream tokens = new CommonTokenStream(lex);
+		
+		// Output our tokens to the parser
+        SimpleASMParser g = new SimpleASMParser(tokens);
+        
+        // Parse...
+        try
+        {
+        	// "program" is our first rule in our grammar, so let's parse our code beginning with this rule
+        	g.program();
+        } catch (RecognitionException e)
+        {
+        	e.printStackTrace();
+        }
 	}
 
 }
